2c1728d92a62bea963994c990ccf814f803668c1
nenya1840 <nenya1840@gmail.com>
2020-08-28 09:44:00 +0800

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ export const UID_KEY = 'weibo_app_uid';
 export const getUid = () => localStorage.getItem(UID_KEY);
 
 export const APP_KEY = '3696852244';
-export const APP_SECRET = '52eaa55c068e880f58b81cab494f819e';
+export const APP_SECRET = 'xxx';
 export const REDIRECT_URI = encodeURIComponent('http://baidu.com:3000/login');
 
 export const LOGIN_URL = `https://api.weibo.com/oauth2/authorize?client_id=${APP_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=email`;
